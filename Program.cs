b0991b564a4b5eb7a9624e681600e66f1ed87028
vnic1920 <163110739+vnic1920@users.noreply.github.com>
2025-10-27 12:15:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using CRMSystem.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+// Custom Services
+builder.Services.AddScoped<IKundeService, KundeService>();
+
+var app = builder.Build();
+
+// Pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
